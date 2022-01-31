3e5ae55570789ac30cb62c12d71284d9aca61f0b
Agata Pietralik <agata.pietralik@student.wat.edu.pl>
2022-01-31 16:11:31 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
+  <Navbar />
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <button 
+  class="btn btn-primary" 
+  data-bs-target="#collapseTarget" 
+  data-bs-toggle="collapse">
+  Bootstrap collapse
+</button>
+<div class="collapse py-2" id="collapseTarget">
+  This is the toggle-able content!
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  }
+};
+
+</script>
+
+<style>
+
+</style>
